283db5420c0d1d8c1d247ade728f5852466b11eb
FrantiskaM <frantiska.malucka@gmail.com>
2024-06-26 18:38:18 +0200

    added JS function to date and time

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,26 @@
-let angelesElement = document.querySelector("#angeles");
-let angelesDateElement = angelesElement.querySelector(".date");
-angelesElement.innerHTML = "Happylll";
+function updateTime() {
+  // Los Angeles
+  let angelesElement = document.querySelector("#angeles");
+  let angelesDateElement = angelesElement.querySelector(".date");
+  let angelesTimeElement = angelesElement.querySelector(".time");
+  let angelesTime = moment().tz("America/Los_Angeles");
+
+  angelesDateElement.innerHTML = angelesTime.format("MMMM Do YYYY");
+  angelesTimeElement.innerHTML = angelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  // Sydney
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  let sydneyTime = moment().tz("Australia/Sydney");
+
+  sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
+  sydneyTimeElement.innerHTML = sydneyTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
